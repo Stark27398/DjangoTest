e7890b7f99e6e1df032bab75420971f28e0b692e
Stark27398 <34678115+Stark27398@users.noreply.github.com>
2019-09-09 12:32:39 +0530

    Update settings.py

--- djangoTest/settings.py
+++ djangoTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+vw_gbgv+u61!*kns-zlq*dzj*5+#+&#2o0z3chh^b*x+fm^n$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
